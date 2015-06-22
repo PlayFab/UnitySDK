96aa59129e6ce452f6ea7715b348053c972809cc
Jeremy Friesen <jeremy@playfab.com>
2015-06-22 15:35:55 -0700

    Spiffed up android plugin build script to copy in Unity classes if they're no already there

--- PluginSource/Android/build.sh
+++ PluginSource/Android/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-set -x
+set -xe
+if [ ! -e "libs/UnityPlayerClasses.jar" ];
+then
+  cp /Applications/Unity/Unity.app/Contents/PlaybackEngines/AndroidPlayer/release/bin/classes.jar libs/UnityPlayerClasses.jar
+fi
 ant jar
 cp release/PlayFabUnityAndroid.jar ../../PlayFabClientSample/Assets/Plugins/Android
