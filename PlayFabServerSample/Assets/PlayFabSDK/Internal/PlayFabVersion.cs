dfa82868e41b843ceea5841e22c1f6f6d5a17482
pgilmorepf <paul@playfab.com>
2015-09-01 14:09:29 -0700

    Updating all SDKs after changes to API_Specs

--- PlayFabServerSample/Assets/PlayFabSDK/Internal/PlayFabVersion.cs
+++ PlayFabServerSample/Assets/PlayFabSDK/Internal/PlayFabVersion.cs
@@ -3,7 +3,7 @@ namespace PlayFab.Internal
 {
 	public class PlayFabVersion
 	{
-		public static string ApiRevision = "1.4.20150824";
+		public static string ApiRevision = "1.5.20150901";
 		public static string SdkRevision = "1.0.6";
 
 		public static string getVersionString()
