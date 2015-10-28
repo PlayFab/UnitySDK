01c34692aafedd756ab9ff36bedc460177cbd193
pgilmorepf <paul@playfab.com>
2015-10-27 17:51:33 -0700

    Utilize the testTitleData.json file in the example project (to hide the dev secret key during the demo, and make things a little faster.

--- PlayFabCombinedTestingSample/Assets/PlayFabSDK/Examples/GUI/Client/C_LoginExampleGui.cs
+++ PlayFabCombinedTestingSample/Assets/PlayFabSDK/Examples/GUI/Client/C_LoginExampleGui.cs
@@ -1,14 +1,26 @@
+using PlayFab.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 namespace PlayFab.Examples.Client
 {
     public class C_LoginExampleGui : PfExampleGui
     {
-        public string titleId = "Set your titleId here";
-        public string devSecretKey = "Set your title secret key here";
+        public string titleDataFileName = "C:/depot/pf-main/tools/SDKBuildScripts/testTitleData.json";
 
-        public string userName = "test username"; // Pick an existing valid username for this title
-        public string email = "test@email.com"; // The email assigned to the user above
-        public string password = "test password"; // The password for the user above
+        [Tooltip("Set your titleId here")]
+        public string titleId;
+        [Tooltip("Set your title secret key here")]
+        public string devSecretKey;
+
+        [Tooltip("Login username")]
+        public string userName; // Pick an existing valid username for this title
+        [Tooltip("Valid email for above user")]
+        public string email; // The email assigned to the user above
+        [Tooltip("Valid password for above user")]
+        public string password; // The password for the user above
 
         #region Unity GUI
         public override void OnExampleGUI(ref int rowIndex)
@@ -16,7 +28,45 @@ namespace PlayFab.Examples.Client
             bool isLoggedIn = PlayFabClientAPI.IsClientLoggedIn();
 
             // Login
-            Button(!isLoggedIn, rowIndex, 0, "Login", () => { LoginExample.LoginWithEmail(titleId, devSecretKey, email, password); });
+            Button(!isLoggedIn, rowIndex, 0, "Login", Login);
+        }
+
+        public void Login()
+        {
+            if (string.IsNullOrEmpty(titleId) || string.IsNullOrEmpty(devSecretKey))
+            {
+                Debug.Log("Logging in using TitleData info");
+
+                bool success = true;
+                if (!File.Exists(titleDataFileName))
+                {
+                    Console.WriteLine("Loading testSettings file failed: " + titleDataFileName);
+                    Console.WriteLine("From: " + Directory.GetCurrentDirectory());
+                    return;
+                }
+
+                string hiddenSecretKey; // Don't want to display this to the inspector if we use this mode
+                string testInputsFile = File.ReadAllText(titleDataFileName);
+                var serializer = JsonSerializer.Create(PlayFab.Internal.Util.JsonSettings);
+                var testInputs = serializer.Deserialize<Dictionary<string, string>>(new JsonTextReader(new StringReader(testInputsFile)));
+
+                success &= testInputs.TryGetValue("titleId", out titleId);
+                success &= testInputs.TryGetValue("developerSecretKey", out hiddenSecretKey);
+
+                success &= testInputs.TryGetValue("userName", out userName);
+                success &= testInputs.TryGetValue("userEmail", out email);
+                success &= testInputs.TryGetValue("userPassword", out password);
+
+                if (success)
+                    LoginExample.LoginWithEmail(titleId, hiddenSecretKey, email, password);
+                else
+                    Debug.Log("TitleData file not formatted correctly.");
+            }
+            else
+            {
+                Debug.Log("Logging in using provided info");
+                LoginExample.LoginWithEmail(titleId, devSecretKey, email, password);
+            }
         }
         #endregion Unity GUI
     }
