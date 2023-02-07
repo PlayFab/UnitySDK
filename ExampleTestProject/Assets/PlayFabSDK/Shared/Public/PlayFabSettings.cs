2c2b3a9bd6f321a9919ccd0a21a51d794af9598b
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2023-02-07 00:16:33 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#230206

--- ExampleTestProject/Assets/PlayFabSDK/Shared/Public/PlayFabSettings.cs
+++ ExampleTestProject/Assets/PlayFabSDK/Shared/Public/PlayFabSettings.cs
@@ -43,9 +43,9 @@ namespace PlayFab
         /// </summary>
         public static readonly PlayFabAuthenticationContext staticPlayer = new PlayFabAuthenticationContext();
 
-        public const string SdkVersion = "2.157.230123";
+        public const string SdkVersion = "2.158.230206";
         public const string BuildIdentifier = "adobuild_unitysdk_167";
-        public const string VersionString = "UnitySDK-2.157.230123";
+        public const string VersionString = "UnitySDK-2.158.230206";
 
         public const string DefaultPlayFabApiUrl = "playfabapi.com";
 
