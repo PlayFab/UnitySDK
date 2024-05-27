10b263a168f1b1aef52ff9cbcd511d60df045fa5
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2024-05-27 22:17:33 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#240524

--- ExampleMacProject/Assets/PlayFabEditorExtensions/Editor/Scripts/Utils/PlayFabEditorVersion.cs
+++ ExampleMacProject/Assets/PlayFabEditorExtensions/Editor/Scripts/Utils/PlayFabEditorVersion.cs
@@ -1,1 +1,1 @@
-namespace PlayFab.PfEditor { public static partial class PlayFabEditorHelper { public static string EDEX_VERSION = "2.194.240514"; } }
+namespace PlayFab.PfEditor { public static partial class PlayFabEditorHelper { public static string EDEX_VERSION = "2.195.240524"; } }
